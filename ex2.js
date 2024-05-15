1bd5d046a78ed2ce20a237377a0b21b98e2f75b7
04_Apiwat (Tee/ตี๋) <apiwat.pienwisetkaew@gmail.com>
2024-05-15 14:26:42 +0700

    first-commit

---
+++ ex2.js
@@ -0,0 +1,17 @@
+
+//Exercise #2.1//
+
+let name =['Sofia','David','Juan'];
+
+//Exercise #2.2//
+
+name.push('Sara','Augustin');
+name.shift();
+console.log(name);
+
+//Exercise #2.3//
+
+
+name.splice(name.indexOf('David') + 1, 0, 'Renata');
+name.push('Elena');
+console.log(name);
